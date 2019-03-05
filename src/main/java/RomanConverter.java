a151b686e97570891371f8eeea611c53f5803969
Mazda Fahandezh Saadi <mazda.fahandezh.saadi@msg-gillardon.de>
2019-03-05 12:38:01 +0100

    add empty classes

---
+++ src/main/java/RomanConverter.java
@@ -0,0 +1,7 @@
+/**
+ * <br><br>
+ * <b>Copyright © BSM BankingSysteme und Managementberatung GmbH, 2018</b>
+ * <br><br>
+ */
+public class RomanConverter {
+}
---
+++ src/test/java/RomanConverterTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <br><br>
+ * <b>Copyright © BSM BankingSysteme und Managementberatung GmbH, 2018</b>
+ * <br><br>
+ */
+class RomanConverterTest {
+
+}
