4dde274f9ee2670095e19762c43c2f2d598a2b32
Mazda Fahandezh Saadi <mazda.fahandezh.saadi@msg-gillardon.de>
2019-03-05 14:03:13 +0100

    add first implementation

--- src/main/java/RomanConverter.java
+++ src/main/java/RomanConverter.java
@@ -1,7 +1,13 @@
 /**
- * <br><br>
- * <b>Copyright © BSM BankingSysteme und Managementberatung GmbH, 2018</b>
- * <br><br>
+ * <br>
+ * <br>
+ * <b>Copyright © BSM BankingSysteme und Managementberatung GmbH, 2018</b> <br>
+ * <br>
  */
-public class RomanConverter {
+public class RomanConverter
+{
+	public String convert(final int arabicNumber)
+	{
+		return "I";
+	}
 }
--- src/test/java/RomanConverterTest.java
+++ src/test/java/RomanConverterTest.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -6,5 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * <br><br>
  */
 class RomanConverterTest {
-
+	@Test
+	@DisplayName("Covert 1 to I Roman")
+	void convert_ArabicNumber_Is_One() {
+		RomanConverter romanConverter = new RomanConverter();
+		String one = romanConverter.convert(1);
+		Assertions.assertEquals("I", one);
+	}
 }
